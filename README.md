ce99866be361d79ac120efcb74f234c27aa9986e
panW <w.hodlik@wp.pl>
2025-05-07 12:48:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# besos-front
+ frontend dla projektu besos, zrobiony w silniku godot bo moge XD
---
+++ globals/scene_changer.gd
@@ -0,0 +1,4 @@
+extends CanvasLayer
+
+func change_scene(path):
+	get_tree().change_scene_to_file(path)
