a30672124878e826591784dea04c6d2094ab4505
panW <w.hodlik@wp.pl>
2025-05-29 20:44:18 +0200

    dodano nowe widoki, zautomatyzowano proces tworzenia
    doszły widoki dla endpointów /zaliczenia i /przedmioty. dodatkowo wprowadzono rozwiązania mające na celu ułatwić tworzenie nowych widoków przy mniejszej ilości kodu

--- views/main_menu.gd
+++ views/main_menu.gd
@@ -6,3 +6,9 @@ func _on_button_kierunki_studenta_pressed():
 
 func _on_button_oceny_pressed():
 	SceneChanger.change_scene("res://views/oceny.tscn")
+
+func _on_button_zaliczenia_pressed():
+	SceneChanger.change_scene("res://views/zaliczenia.tscn")
+
+func _on_button_przedmioty_pressed():
+	SceneChanger.change_scene("res://views/przedmioty.tscn")
